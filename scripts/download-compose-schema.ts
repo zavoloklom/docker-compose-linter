e89549c5725bb3e3db9aaf199faa9f6640e8578a
Sergey Kupletsky <s.kupletsky@gmail.com>
2024-12-14 12:09:57 +0100

    chore: rewrite download-compose-schema script to TS

---
+++ scripts/download-compose-schema.ts
@@ -0,0 +1,19 @@
+import { createWriteStream } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { get } from 'node:https';
+import { fileURLToPath } from 'node:url';
+
+const url = 'https://raw.githubusercontent.com/compose-spec/compose-spec/main/schema/compose-spec.json';
+const outputPath = join(dirname(fileURLToPath(import.meta.url)), '../schemas/compose.schema.json');
+
+get(url, (response) => {
+  const fileStream = createWriteStream(outputPath);
+  response.pipe(fileStream);
+
+  fileStream.on('finish', () => {
+    fileStream.close();
+    console.log('File downloaded successfully');
+  });
+}).on('error', (error) => {
+  console.error('Error downloading the file:', error.message);
+});
