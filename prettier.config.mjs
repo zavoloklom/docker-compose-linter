fa3d0b922e75c0b296a06e5b61da13f852d5b079
Sergey Kupletsky <s.kupletsky@gmail.com>
2025-07-14 22:50:22 +0200

    chore: switch eslint and prettier to @zavoloklom/shareable-configs

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+import prettierConfig from '@zavoloklom/prettier-config';
+
+const config = {
+  ...prettierConfig,
+};
+
+// eslint-disable-next-line import/no-default-export
+export default config;
